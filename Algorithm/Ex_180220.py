691f2f3090437e1c3c7660478467877816753240
hoang <hoangbui1699@gmail.com>
2020-02-18 16:07:09 +0700

    Ex 18/2/2020

--- Algorithm/Ex_180220.py
+++ Algorithm/Ex_180220.py
@@ -1,12 +1,12 @@
-list1 = [1, 5]
+list = [4, 7, 1, -3, 2]
 
 
-def two_sum(list1, k):
-    for i in list1:
-        for y in list1:
+def two_sum(list, k):
+    for i in list:
+        for y in list:
             if i + y == k:
                 return True
     return False
 
 
-print(two_sum(list1, 6))
+print(two_sum(list, 5))
