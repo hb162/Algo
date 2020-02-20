3369cb0930f47d1c4f83201f6de30196f219cb1c
hoang <hoangbui1699@gmail.com>
2020-02-20 21:21:23 +0700

    EX 20/02/20

--- Algorithm/Ex_200220_Facebook.py
+++ Algorithm/Ex_200220_Facebook.py
@@ -5,19 +5,14 @@ class BST:
         self.value = value
 
 
-def add_node(root, node):
-    if root is None:
-        root = node
-    if root.value < node.value:
-        if root.right is None:
-            root.right = node
-        else:
-            add_node(root.right, node)
-    elif root.value > node.value:
-        if root.left is None:
-            root.left = node
-        else:
-            add_node(root.left, node)
+def add_node(node, value):
+    if node is None:
+        return BST(value)
+    if value < node.value:
+        node.left = add_node(node.left, value)
+    else:
+        node.right = add_node(node.right, value)
+    return node
 
 
 def check(tree1, tree2):
@@ -51,21 +46,21 @@ def postorder(root):
 
 
 b1 = BST(10)
-add_node(b1, BST(1))
-add_node(b1, BST(4))
-add_node(b1, BST(6))
-add_node(b1, BST(7))
-add_node(b1, BST(3))
-add_node(b1, BST(8))
-add_node(b1, BST(20))
+b1 = add_node(b1, 5)
+b1 = add_node(b1, 9)
+b1 = add_node(b1, 0)
+b1 = add_node(b1, 3)
+b1 = add_node(b1, 7)
+b1 = add_node(b1, 6)
+b1 = add_node(b1, 20)
 
 b2 = BST(10)
-add_node(b2, BST(1))
-add_node(b2, BST(4))
-add_node(b2, BST(6))
-add_node(b2, BST(7))
-add_node(b2, BST(3))
-add_node(b2, BST(8))
-add_node(b2, BST(20))
+b2 = add_node(b2, 5)
+b2 = add_node(b2, 9)
+b2 = add_node(b2, 0)
+b2 = add_node(b2, 3)
+b2 = add_node(b2, 7)
+b2 = add_node(b2, 6)
+b2 = add_node(b2, 20)
 print('Equal?', check(b1, b2))
 
